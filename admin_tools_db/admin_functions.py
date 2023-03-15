7226a4f720f5871b79ace1258f6d32bdaac1c95e
dmnburrows <dominicburrows@hotmail.co.uk>
2023-03-14 19:10:10 -0700

    moving files into subfolder

---
+++ admin_tools_db/admin_functions.py
@@ -0,0 +1,15 @@
+###############################
+
+def prac(x,y):
+    """
+        This function is a practice. 
+        
+   
+    Inputs:
+    
+    Outputs:
+     
+        """
+
+    z=x+y
+    return(z,y)

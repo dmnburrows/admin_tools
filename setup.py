0a3ea042abf0b6ad38210b81275dbbbe567c7798
Dominic Burrows <45342427+dmnburrows@users.noreply.github.com>
2023-03-14 20:26:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
-from setuptools import setup, find_packages
-
-with open('README.md') as readme_file:
-    README = readme_file.read()
-
-with open('HISTORY.md') as history_file:
-    HISTORY = history_file.read()
-
-setup_args = dict(
-    name='admin_tools_db',
-    version='0.1',
-    description='Simple functions for admin jobs in python',
-    long_description_content_type="text/markdown",
-    long_description=README + '\n\n' + HISTORY,
-    license='MIT',
-    packages=find_packages(),
-    author='Dominic Burrows',
-    author_email='dominicburrows@hotmail.co.uk',
-    keywords=[''],
-    url='https://github.com/dmnburrows/admin_tools_db',
-    download_url='https://pypi.org/project/admin_tools_db/'
-)
-
-install_requires = [
-                 'numpy'
-]
-
-
 from distutils.core import setup
 setup(
   name = 'admin_tools_db',         # How you named your package folder (MyLib)

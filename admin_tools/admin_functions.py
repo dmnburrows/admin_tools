fddca3771a4de910d13d245c30899003124eacd4
dmnburrows <dominicburrows@hotmail.co.uk>
2023-05-05 21:38:46 +0000

    Updating sort function

--- admin_tools/admin_functions.py
+++ admin_tools/admin_functions.py
@@ -1,24 +1,22 @@
 
 #================================
-def sort_list(list1, list2):
+def sort_2list(list1, list2):
 #=================================
     """
     This function sorts one list by another list. It can work with 1d arrays or lists. 
 
     Input:
-    list1: list/array to be sorted
-    list2: list/array to sort by
+    list1: list/array to sort by
+    list2: list/array to be sorted
 
     Output:
-    z: list1 sorted by list 2
-    sorted(list2): list2 sorted
+    sorted(list1): list1 sorted by itself
+    z: list2 sorted by list 1
     """
 
-
-    zipped_pairs = zip(list2, list1)
+    zipped_pairs = zip(list1, list2)
  
     z = [x for _, x in sorted(zipped_pairs)]
  
-    return z, sorted(list2)
-
+    return sorted(list1), z
 
